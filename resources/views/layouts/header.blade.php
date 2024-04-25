0c3e8b9ad266f54bb12ec2af5f08642687de6e43
lilipey <lisa.peyron@my-digital-school.org>
2024-04-25 14:05:04 +0200

    1st header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,14 @@
+<header id="header">
+<nav>
+    <a href=""><img src="storage/img/logo-blanc.png" alt=""></a>
+    <ul>
+        <li><a href="/" class="button">Home</a></li>
+        <li><a href="/about" class="button">About</a></li>
+        <li><a href="/contact" class="button">Contact</a></li>
+    </ul>
+    <ul>
+        <li><a href="/login" class="button">Login</a></li>
+        <li><a href="/register" class="button">Register</a></li>
+    </ul>
+</nav>
+</header>
