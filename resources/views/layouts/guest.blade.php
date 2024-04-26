957fb4ca411e8158792ebdb1588c13d35248a6e9
Eric Mai <eric.mai@ekami.ch>
2024-04-26 10:18:02 +0200

    changed vite file path

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="storage/css/app-DaN8u4V-.css">
+        <script src="storage/js/app-CaTRVF7O.js"></script>
+
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 dark:bg-gray-900" style="position:relative; top:50px;">
